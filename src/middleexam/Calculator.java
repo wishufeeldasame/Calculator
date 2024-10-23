c558473984d650103a1f9f3543eb985e02a105ac
wishufeeldasame <bubblegrape@kakao.com>
2024-10-23 13:04:30 +0900

    GUI 구현

--- src/middleexam/Calculator.java
+++ src/middleexam/Calculator.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
  * iOS 사칙연산 계산기 프로그램
  * 
  * @author Suh Jang Ho
- * @version 0
+ * @version 0.1
  * 
  * @created 2024.10.23
  * @updated 2024.10.23
@@ -23,8 +23,44 @@ import java.awt.event.ActionListener;
     private double num1, num2, result; // 계산에 사용할 변수
 
     public Calculator(){
-        
+        // 프로그램의 창 크기와, 종료, 레이아웃 설정
+        setTitle("계산기");
+        setSize(300,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        // 계산 결과와 입력을 표시해줄 텍스트 필드 정의
+        display = new JTextField();
+        display.setEditable(false);
+        display.setHorizontalAlignment(SwingConstants.RIGHT);
+        add(display, BorderLayout.NORTH);
+
+        // 버튼 정의 및 패널에 추가
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(5,4,5,5));
+
+        String[] buttons ={
+            "C","±","%","÷",
+            "7","8","9","x",
+            "4","5","6","-",
+            "1","2","3","+",
+            "0",".","="
+        };
+
+        for(String label : buttons){
+            JButton button = new JButton(label);
+            button.setFocusable(false);
+            button.addActionListener(null);
+            panel.add(button);
+        }
+
+        add(panel, BorderLayout.CENTER);
+        setVisible(true);
     }
+
+    // 버튼 클릭 이벤트 처리
+    //@Override
+    //public void actionPerformed(ActionEvent e){  }
 
     public static void main(String[] args) {
         new Calculator();
